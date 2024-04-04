c6d1bf1646653976d2d3b7a17a35a3fefc39f757
Huy Nguyễn <huynguyen287123@gmail.com>
2024-04-04 10:11:47 +0700

    feat: add search product by name feature

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,27 +8,36 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category'
-  
+
   constructor(private httpClient: HttpClient) { }
-  
+
   getProductList(categoryId: number): Observable<Product[]> {
-    const searcUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(keyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`;
     
-    return this.httpClient.get<GetResponseProducts>(searcUrl).pipe(
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
-      );
+    );
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
-      );
+    );
   }
 }
-  
+
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
